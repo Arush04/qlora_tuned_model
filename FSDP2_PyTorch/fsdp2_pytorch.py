d78306f2ef954d8a3be608e4abb2722277c07dca
Arush04 <sharmaarush591@gmail.com>
2025-07-05 03:27:42 +0530

    changes commentd

--- FSDP2_PyTorch/fsdp2_pytorch.py
+++ FSDP2_PyTorch/fsdp2_pytorch.py
@@ -9,83 +9,83 @@ from transformers import (
     AutoConfig, AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 )
 from datasets import load_dataset
-from functools import partial
+# from functools import partial
 
-def create_prompt_formats(sample):
-    """
-    Format various fields of the sample ('instruction','output')
-    Then concatenate them using two newline characters 
-    :param sample: Sample dictionnary
-    """
-    INTRO_BLURB = "Below is an instruction that describes a task. Write a response that appropriately completes the request."
-    INSTRUCTION_KEY = "### Instruct: Summarize the below conversation."
-    RESPONSE_KEY = "### Output:"
-    END_KEY = "### End"
+# def create_prompt_formats(sample):
+#     """
+#     Format various fields of the sample ('instruction','output')
+#     Then concatenate them using two newline characters 
+#     :param sample: Sample dictionnary
+#     """
+#     INTRO_BLURB = "Below is an instruction that describes a task. Write a response that appropriately completes the request."
+#     INSTRUCTION_KEY = "### Instruct: Summarize the below conversation."
+#     RESPONSE_KEY = "### Output:"
+#     END_KEY = "### End"
 
-    blurb = f"\n{INTRO_BLURB}"
-    instruction = f"{INSTRUCTION_KEY}"
-    input_context = f"{sample['dialogue']}" if sample["dialogue"] else None
-    response = f"{RESPONSE_KEY}\n{sample['summary']}"
-    end = f"{END_KEY}"
+#     blurb = f"\n{INTRO_BLURB}"
+#     instruction = f"{INSTRUCTION_KEY}"
+#     input_context = f"{sample['dialogue']}" if sample["dialogue"] else None
+#     response = f"{RESPONSE_KEY}\n{sample['summary']}"
+#     end = f"{END_KEY}"
 
-    parts = [part for part in [blurb, instruction, input_context, response, end] if part]
+#     parts = [part for part in [blurb, instruction, input_context, response, end] if part]
 
-    formatted_prompt = "\n\n".join(parts)
-    sample["text"] = formatted_prompt
+#     formatted_prompt = "\n\n".join(parts)
+#     sample["text"] = formatted_prompt
 
-    return sample
-
-# SOURCE https://github.com/databrickslabs/dolly/blob/master/training/trainer.py
-def get_max_length(model):
-    conf = model.config
-    max_length = None
-    for length_setting in ["n_positions", "max_position_embeddings", "seq_length"]:
-        max_length = getattr(model.config, length_setting, None)
-        if max_length:
-            print(f"Found max lenth: {max_length}")
-            break
-    if not max_length:
-        max_length = 1024
-        print(f"Using default max length: {max_length}")
-    return max_length
-
-
-def preprocess_batch(batch, tokenizer, max_length):
-    """
-    Tokenizing a batch
-    """
-    return tokenizer(
-        batch["text"],
-        max_length=max_length,
-        truncation=True,
-    )
+#     return sample
 
 # SOURCE https://github.com/databrickslabs/dolly/blob/master/training/trainer.py
-def preprocess_dataset(tokenizer: AutoTokenizer, max_length: int,seed, dataset):
-    """Format & tokenize it so it is ready for training
-    :param tokenizer (AutoTokenizer): Model Tokenizer
-    :param max_length (int): Maximum number of tokens to emit from tokenizer
-    """
+# def get_max_length(model):
+#     conf = model.config
+#     max_length = None
+#     for length_setting in ["n_positions", "max_position_embeddings", "seq_length"]:
+#         max_length = getattr(model.config, length_setting, None)
+#         if max_length:
+#             print(f"Found max lenth: {max_length}")
+#             break
+#     if not max_length:
+#         max_length = 1024
+#         print(f"Using default max length: {max_length}")
+#     return max_length
 
-    # Add prompt to each sample
-    print("Preprocessing dataset...")
-    dataset = dataset.map(prompt_helper_function)#, batched=True)
 
-    # Apply preprocessing to each batch of the dataset & and remove 'instruction', 'context', 'response', 'category' fields
-    _preprocessing_function = partial(preprocess_batch, max_length=max_length, tokenizer=tokenizer)
-    dataset = dataset.map(
-        _preprocessing_function,
-        batched=True,
-        remove_columns=['Context', 'Response'],
-    )
+# def preprocess_batch(batch, tokenizer, max_length):
+#     """
+#     Tokenizing a batch
+#     """
+#     return tokenizer(
+#         batch["text"],
+#         max_length=max_length,
+#         truncation=True,
+#     )
 
-    # Filter out samples that have input_ids exceeding max_length
-    dataset = dataset.filter(lambda sample: len(sample["input_ids"]) < max_length)
+# SOURCE https://github.com/databrickslabs/dolly/blob/master/training/trainer.py
+# def preprocess_dataset(tokenizer: AutoTokenizer, max_length: int,seed, dataset):
+#     """Format & tokenize it so it is ready for training
+#     :param tokenizer (AutoTokenizer): Model Tokenizer
+#     :param max_length (int): Maximum number of tokens to emit from tokenizer
+#     """
 
-    # Shuffle dataset
-    dataset = dataset.shuffle(seed=seed)
+#     # Add prompt to each sample
+#     print("Preprocessing dataset...")
+#     dataset = dataset.map(prompt_helper_function)#, batched=True)
 
-    return dataset
+#     # Apply preprocessing to each batch of the dataset & and remove 'instruction', 'context', 'response', 'category' fields
+#     _preprocessing_function = partial(preprocess_batch, max_length=max_length, tokenizer=tokenizer)
+#     dataset = dataset.map(
+#         _preprocessing_function,
+#         batched=True,
+#         remove_columns=['Context', 'Response'],
+#     )
+
+#     # Filter out samples that have input_ids exceeding max_length
+#     dataset = dataset.filter(lambda sample: len(sample["input_ids"]) < max_length)
+
+#     # Shuffle dataset
+#     dataset = dataset.shuffle(seed=seed)
+
+#     return dataset
 
 def create_prompt(sample):
     prompt = (
